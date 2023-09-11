47298e098b4d0efbc15276e668b41bd2faedf88e
Natan <mouranatan933@gmail.com>
2023-09-11 11:55:41 -0300

    hi[

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,69 @@
-import { useEffect } from "react";
-import { Camera,CameraType } from "expo-camera";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { Camera, CameraType, FaceDetectionResult } from "expo-camera";
+import { ImageSourcePropType, View } from "react-native";
 import { styles } from "./styles";
+import * as FaceDetector from "expo-face-detector"
+import Animated, { useSharedValue, useAnimatedStyle } from "react-native-reanimated"
+import neutralImg from "../../assets/neutral.png"
+import smileImg from "../../assets/smile.png"
+import winkingImg from "../../assets/winking.png"
 const Home = () => {
+  const [faceDetected, setFaceDetected] = useState(false)
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [emoji, setEmoji] = useState<ImageSourcePropType>(neutralImg);
 
-  useEffect(()=> {
+
+  const facesValues = useSharedValue({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0
+  })
+  const handleFacesDetected = ({ faces }: FaceDetectionResult) => {
+    const face = faces[0] as any;
+
+    if (face) {
+      const { size, origin } = face.bounds;
+
+      facesValues.value = {
+        width: size.width,
+        height: size.height,
+        x: origin.x,
+        y: origin.y
+
+      }
+      setFaceDetected(true)
+
+      if (face.smilingProbability > 0.5) {
+        setEmoji(smileImg)
+      } else if (face.leftEyeOpenProbability < 0.5 && face.rightEyeOpenProbability > 0.5) {
+        setEmoji(winkingImg)
+      }
+
+      else {
+        setEmoji(neutralImg)
+      }
+
+    } else {
+      setFaceDetected(false)
+    }
+
+  }
+
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    position: 'absolute',
+    zIndex: 1,
+    width: facesValues.value.width,
+    height: facesValues.value.height,
+    transform: [
+      { translateX: facesValues.value.x },
+      { translateY: facesValues.value.y },
+    ]
+
+  }))
+
+  useEffect(() => {
     requestPermission();
   }, []);
 
@@ -14,8 +72,17 @@ const Home = () => {
   }
   return (
     <View style={styles.container}>
+      {faceDetected && <Animated.Image style={animatedStyle} source={emoji} />}
       <Camera style={styles.camera}
-      type={CameraType.front}
+        type={CameraType.front}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.all,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}
       />
     </View>
   );
