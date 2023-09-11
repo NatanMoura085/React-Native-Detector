221494a3f7cfadf604a035a9a600306339b01816
Natan <mouranatan933@gmail.com>
2023-09-11 18:31:05 -0300

    sure

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,31 +1,39 @@
 import { useEffect, useState } from "react";
 import { Camera, CameraType, FaceDetectionResult } from "expo-camera";
-import { ImageSourcePropType, View } from "react-native";
+import { Image, ImageSourcePropType, View } from "react-native";
 import { styles } from "./styles";
-import * as FaceDetector from "expo-face-detector"
-import Animated, { useSharedValue, useAnimatedStyle } from "react-native-reanimated"
-import neutralImg from "../../assets/neutral.png"
-import smileImg from "../../assets/smile.png"
-import winkingImg from "../../assets/winking.png"
+import * as FaceDetector from 'expo-face-detector';
+import { useSharedValue, useAnimatedStyle } from "react-native-reanimated"
+import Animated from "react-native-reanimated"
+import neutral2 from "../assets/neutral2.png"
+import smileImg from "../assets/smile.png"
+import winkingImg from "../assets/winking.png"
+import sleepingImg from "../assets/sleeping.png"
+import poutingImg from "../assets/pouting.png"
 const Home = () => {
   const [faceDetected, setFaceDetected] = useState(false)
   const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [emoji, setEmoji] = useState<ImageSourcePropType>(neutralImg);
+  const [emoji, setEmoji] = useState<ImageSourcePropType>(neutral2);
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
 
 
-  const facesValues = useSharedValue({
+  const faceValues = useSharedValue({
     width: 0,
     height: 0,
     x: 0,
     y: 0
   })
   const handleFacesDetected = ({ faces }: FaceDetectionResult) => {
+    console.log(faces)
     const face = faces[0] as any;
 
     if (face) {
       const { size, origin } = face.bounds;
 
-      facesValues.value = {
+      faceValues.value = {
         width: size.width,
         height: size.height,
         x: origin.x,
@@ -36,12 +44,18 @@ const Home = () => {
 
       if (face.smilingProbability > 0.5) {
         setEmoji(smileImg)
-      } else if (face.leftEyeOpenProbability < 0.5 && face.rightEyeOpenProbability > 0.5) {
+      } else if (face.leftEyeOpenProbability > 0.5 && face.rightEyeOpenProbability < 0.5) {
         setEmoji(winkingImg)
+      }
+      else if (face.leftEyeOpenProbability < 0.2 && face.rightEyeOpenProbability < 0.2) {
+        setEmoji(sleepingImg)
+      } else if (face.emotions?.angry > 0.5) { 
+        setEmoji(poutingImg);
+ 
       }
 
       else {
-        setEmoji(neutralImg)
+        setEmoji(neutral2)
       }
 
     } else {
@@ -53,19 +67,20 @@ const Home = () => {
 
   const animatedStyle = useAnimatedStyle(() => ({
     position: 'absolute',
-    zIndex: 1,
-    width: facesValues.value.width,
-    height: facesValues.value.height,
+    zIndex: 20,
+    // flex:1,
+    objectFit: 'contain',
+    width: faceValues.value.width,
+    height: faceValues.value.height,
     transform: [
-      { translateX: facesValues.value.x },
-      { translateY: facesValues.value.y },
-    ]
-
+      { translateX: faceValues.value.x },
+      { translateY: faceValues.value.y },
+    ],
+    // borderColor: 'blue',
+    // borderWidth: 6
   }))
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
+
 
   if (!permission?.granted) {
     return;
--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -3,10 +3,13 @@ import {StyleSheet} from "react-native"
 export const styles = StyleSheet.create({
 
     container:{
-        flexGrow:1
+        flexGrow:1,
+        zIndex:-1,
     },
 
     camera:{
+        bottom:0,
+        zIndex:1,
         flex:1
     }
 
